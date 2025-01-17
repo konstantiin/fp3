dc4c8336b2f2dd6d8112ee94dfa1ae48745b9436
konstantiin <Kostiicc@yandex.ru>
2025-01-17 21:32:34 +0300

    fixed points

--- bin/main.ml
+++ bin/main.ml
@@ -1,25 +1,25 @@
 open Reader
 
-type config = { frec : float }
+type config = { freq : float }
 
 let cfg =
   match Array.length Sys.argv with
-  | 1 -> { frec = 1.0 }
-  | 2 -> { frec = Sys.argv.(1) |> float_of_string }
+  | 1 -> { freq = 1.0 }
+  | 2 -> { freq = Sys.argv.(1) |> float_of_string }
   | _ ->
       let _ = Printer.print_msg "one argument supported" in
       exit 0
 
-let rec main_loop points_last cnt config =
+let rec main_loop points_last cnt config is_first =
   let _ = Printer.print_msg "Input next point or command:" in
   match read_command () with
   | exception Invalid_argument s ->
       let _ = Printer.print_msg s in
-      main_loop points_last cnt config
+      main_loop points_last cnt config is_first
   | Exit -> ()
   | Freq fr ->
       let _ = Printer.print_msg "frequency set" in
-      main_loop points_last cnt { frec = fr }
+      main_loop points_last cnt { freq = fr } is_first
   | Point xy ->
       let points = xy :: points_last in
       let ifuncs = Interpolator.apply_data points in
@@ -29,15 +29,20 @@ let rec main_loop points_last cnt config =
             match func.f with
             | None -> Printer.print_not_enough func.name
             | Some f ->
-                let lower_bound, _ = List.nth points (func.win_sz - 1) in
                 let upper_bound, _ = xy in
-                let pred = Utils.get_disc lower_bound upper_bound config.frec in
+                let lower_bound =
+                  if is_first then upper_bound
+                  else
+                    let l, _ = List.nth points_last 0 in
+                    l +. config.freq
+                in
+                let pred = Utils.get_disc lower_bound upper_bound config.freq in
                 let predicted = List.map f pred in
                 Printer.print_points func.name
                   (List.rev (List.combine pred predicted)))
           ifuncs
       in
-      main_loop points (cnt + 1) config
+      main_loop points (cnt + 1) config false
 
-let _ = Printer.print_msg "Usage: exit; <float> <float>"
-let _ = main_loop [] 0 cfg
+let _ = Printer.print_msg "Usage: setFreq <float>; exit; <float> <float>"
+let _ = main_loop [] 0 cfg true
